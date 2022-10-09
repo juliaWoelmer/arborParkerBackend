ed399a21c6e8503e23c436124ffad9eb5493b0e6
Julia <jwoelmer@umich.edu>
2022-10-09 19:54:03 -0400

    port to PORT

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const mysql = require('mysql');
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 const connection = mysql.createConnection({
     host: 'us-cdbr-east-06.cleardb.net',
