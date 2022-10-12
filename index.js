19b0f3f6dfafc06fcf80e18618dd742f7178549a
Julia <jwoelmer@umich.edu>
2022-10-11 22:22:39 -0400

    Removed semicolons and / message

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const app = express();
-const mysql = require('mysql');
+const express = require('express')
+const app = express()
+const mysql = require('mysql')
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 
 const pool = mysql.createPool({
     connectionLimit: 10,
@@ -15,29 +15,30 @@ const pool = mysql.createPool({
 });
 
 app.get('/', function(req, res) {
-    res.send('Hello world')
+    res.send('Welcome to the Arbor Parker server')
 })
 
 app.get('/spots', function(req, res) {
     pool.query('SELECT * FROM Spot', (error, rows) => {
-        if (error) throw error;
+        if (error) throw error
         
         const rowJson = rows.map(spot => {
             return {id: spot.SpotId, isOpen: !!spot.Open}
         });
-        res.json(rowJson);
-        console.log(rows);
+        res.json(rowJson)
+        console.log(rows)
     });
 })
 
 app.put('/spots/:id/set-open-state', function(req, res) {
     const isOpenToNumber = req.body.isOpen === true ? 1: 0
     pool.query(`UPDATE Spot SET Open = ${isOpenToNumber} WHERE SpotId = ${parseInt(req.params.id)}`, (error, rows) => {
-        if (error) throw error;
-    
-        res.json({id: req.id, isOpen: req.body.isOpen});
-        console.log(rows);
-    });
+        if (error) throw error
+        
+        const updatedSpot = {id: req.id, isOpen: req.body.isOpen}
+        res.json(updatedSpot)
+        console.log(updatedSpot)
+    })
 })
 
 app.listen(port);
