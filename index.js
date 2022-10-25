7f4f66a3c36658a7654c7f673311abc4a2e13572
Julia <jwoelmer@umich.edu>
2022-10-24 23:39:21 -0400

    Both queries in one

--- index.js
+++ index.js
@@ -54,16 +54,11 @@ app.get('/user/:id/', function(req, res) {
 })
 
 app.post('/user/add-new-user', function(req, res) {
-    pool.query("INSERT INTO User (Username, Password) VALUES (?, ?)", [req.body.username, req.body.password], (error, rows) => {
+    pool.query("INSERT INTO User (Username, Password) VALUES (?, ?); SELECT LAST_INSERT_ID()", [req.body.username, req.body.password], (error, rows) => {
         if (error) throw error
         
         let insertedUser = {username: req.body.username, password: req.body.password}
-        pool.query("SELECT LAST_INSERT_ID()", (errorId, rowsId) => {
-            if (errorId) throw error
-            
-            insertedUser["id"] = rowsId;
-            res.json(insertedUser)
-        })
+        res.json(insertedUser)
     })
 })
 
