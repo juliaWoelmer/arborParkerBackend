61df7dcae6a60a8977ff1b19991863597bc98858
Julia <jwoelmer@umich.edu>
2022-10-24 23:56:41 -0400

    rows.insertId syntax

--- index.js
+++ index.js
@@ -54,10 +54,10 @@ app.get('/user/:id/', function(req, res) {
 })
 
 app.post('/user/add-new-user', function(req, res) {
-    pool.query("INSERT INTO User (Username, Password) VALUES (?, ?) RETURNING UserId", [req.body.username, req.body.password], (error, rows) => {
+    pool.query("INSERT INTO User (Username, Password) VALUES (?, ?)", [req.body.username, req.body.password], (error, rows) => {
         if (error) throw error
         
-        let insertedUser = {username: req.body.username, password: req.body.password}
+        let insertedUser = {id: rows.insertId, username: req.body.username, password: req.body.password}
         res.json(insertedUser)
     })
 })
