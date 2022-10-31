fa6e38f483789a7e138853b53650015abfa0b59c
Julia <jwoelmer@umich.edu>
2022-10-31 00:40:58 -0400

    Added error handling for all queries

--- index.js
+++ index.js
@@ -21,13 +21,15 @@ app.get('/', function(req, res) {
 // Gets all spots and their availabilities, returns a list of spots in form {id: someId, isOpen, whether or not spot is open}
 app.get('/spots', function(req, res) {
     pool.query('SELECT * FROM Spot', (error, rows) => {
-        if (error) throw error
-        
-        const rowJson = rows.map(spot => {
-            return {id: spot.SpotId, isOpen: !!spot.Open}
-        });
-        res.json(rowJson)
-        console.log(rows)
+        if (error) {
+            res.json(error)
+        } else {
+            const rowJson = rows.map(spot => {
+                return {id: spot.SpotId, isOpen: !!spot.Open}
+            });
+            res.json(rowJson)
+            console.log(rows)
+        } 
     });
 })
 
@@ -35,11 +37,13 @@ app.get('/spots', function(req, res) {
 app.put('/spots/:id/set-open-state', function(req, res) {
     const isOpenToNumber = req.body.isOpen === true ? 1: 0
     pool.query("UPDATE Spot SET Open = ? WHERE SpotId = ?", [isOpenToNumber, parseInt(req.params.id)], (error, rows) => {
-        if (error) throw error
-        
-        const updatedSpot = {id: req.id, isOpen: req.body.isOpen}
-        res.json(updatedSpot)
-        console.log(updatedSpot)
+        if (error) {
+            res.json(error)
+        } else {
+            const updatedSpot = {id: req.id, isOpen: req.body.isOpen}
+            res.json(updatedSpot)
+            console.log(updatedSpot)
+        }
     })
 })
 
@@ -57,22 +61,24 @@ app.put('/spots/:id/set-open-state', function(req, res) {
 // }
 app.get('/user/:id/', function(req, res) {
     pool.query("SELECT * FROM User WHERE UserId = ?", [parseInt(req.params.id)], (error, rows) => {
-        if (error) throw error
-        
-        const rowJson = rows.map(user => {
-            return {
-                id: user.UserId, 
-                username: user.Username, 
-                password: user.Password,
-                firstName: user.FirstName,
-                lastName: user.LastName,
-                email: user.Email,
-                allowStairs: user.AllowStairs,
-                colorTheme: user.ColorTheme
-            }
-        });
-        res.json(rowJson)
-        console.log(rows)
+        if (error) {
+            res.json(error)
+        } else {
+            const rowJson = rows.map(user => {
+                return {
+                    id: user.UserId, 
+                    username: user.Username, 
+                    password: user.Password,
+                    firstName: user.FirstName,
+                    lastName: user.LastName,
+                    email: user.Email,
+                    allowStairs: user.AllowStairs,
+                    colorTheme: user.ColorTheme
+                }
+            });
+            res.json(rowJson)
+            console.log(rows)
+        }
     });
 })
 
@@ -90,22 +96,24 @@ app.get('/user/:id/', function(req, res) {
 // }
 app.get('/user/by-username/:username', function(req, res) {
     pool.query("SELECT * FROM User WHERE Username = ?", [req.params.username.toString()], (error, rows) => {
-        if (error) throw error
-        
-        const rowJson = rows.map(user => {
-            return {
-                id: user.UserId, 
-                username: user.Username, 
-                password: user.Password,
-                firstName: user.FirstName,
-                lastName: user.LastName,
-                email: user.Email,
-                allowStairs: user.AllowStairs,
-                colorTheme: user.ColorTheme
-            }
-        });
-        res.json(rowJson)
-        console.log(rows)
+        if (error) {
+            res.json(error)
+        } else {
+            const rowJson = rows.map(user => {
+                return {
+                    id: user.UserId, 
+                    username: user.Username, 
+                    password: user.Password,
+                    firstName: user.FirstName,
+                    lastName: user.LastName,
+                    email: user.Email,
+                    allowStairs: user.AllowStairs,
+                    colorTheme: user.ColorTheme
+                }
+            });
+            res.json(rowJson)
+            console.log(rows)
+        }
     });
 })
 
@@ -145,14 +153,14 @@ app.put('/user/edit-user/:id', function(req, res) {
             req.body.colorTheme,
             parseInt(req.params.id)
         ], (error, rows) => {
-        if (error) throw error
-        let affectedRows = {affectedRows: rows.affectedRows}
-        res.json(affectedRows)
+        if (error) {
+            res.json(error)
+        } else {
+            let affectedRows = {affectedRows: rows.affectedRows}
+            res.json(affectedRows)
+        }
     })
 })
-
-
-
 
 app.listen(port);
 console.log(`Server is listening on port: ${port}`)
